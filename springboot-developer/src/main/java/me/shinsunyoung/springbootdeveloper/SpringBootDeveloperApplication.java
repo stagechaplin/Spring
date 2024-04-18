7091c386d433eb5ae14ec2970910ee33e24c2078
stagechaplin <https://github.com/stagechaplin/stagechaplin.git>
2024-04-18 17:55:36 +0900

    타임 리프 적용및 시큐리티

--- springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -2,9 +2,10 @@ package me.shinsunyoung.springbootdeveloper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.swing.*;
-
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
--- springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
@@ -5,7 +5,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity // 엔티티로 지정
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -30,5 +36,12 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime updatedAt;
 
 }
